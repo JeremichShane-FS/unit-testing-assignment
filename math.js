989e4e398e1d4afa233576ce966577635fae40e1
shanejeremich <shane.jeremich@gmail.com>
2024-05-13 15:32:45 -0600

    feat: Created 6 math functions

---
+++ math.js
@@ -0,0 +1,9 @@
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let divide = (a, b) => a / b;
+let multiply = (a, b) => a * b;
+
+let sqrt = a => Math.sqrt(a);
+let max = (a, b) => Math.max(a, b);
+
+module.exports = { add, subtract, divide, multiply, sqrt, max };
